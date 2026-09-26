903db2b3d67ff897937200c093a4e226f7dfe1bc
Ryuichi Hoshi <hoshi.ryuichi@gmail.com>
2026-09-26 15:30:05 +0900

    feat(solv-003): implement 7-empty leaf solver with preserved move ordering

--- src/end.c
+++ src/end.c
@@ -1216,6 +1216,165 @@ solve_six_empty( BitBoard my_bits,
 
 
 static int
+solve_seven_empty( BitBoard my_bits,
+		   BitBoard opp_bits,
+		   int sq1,
+		   int sq2,
+		   int sq3,
+		   int sq4,
+		   int sq5,
+		   int sq6,
+		   int sq7,
+		   int alpha,
+		   int beta,
+		   int color,
+		   int disc_diff,
+		   int pass_legal ) {
+  BitBoard new_opp_bits;
+  int score = -INFINITE_EVAL;
+  int in_alpha = alpha;
+  int oppcol = OPP( color );
+  int flipped;
+  int new_disc_diff;
+  int ev;
+  int best_sq = 0;
+
+  INCREMENT_COUNTER( nodes );
+
+#if USE_SHALLOW_TT
+  {
+    HashEntry entry;
+    find_shallow_hash( &entry );
+    if ( (entry.draft == 7) && (entry.flags & ENDGAME_SCORE) ) {
+      if ( (entry.flags & EXACT_VALUE) ||
+	   ((entry.flags & LOWER_BOUND) && entry.eval >= beta) ||
+	   ((entry.flags & UPPER_BOUND) && entry.eval <= alpha) ) {
+	end_best_move = entry.move[0];
+	return entry.eval;
+      }
+    }
+  }
+#endif
+
+#if USE_STABILITY
+  int opp_cnt = non_iterative_popcount( opp_bits );
+  if ( alpha >= 0 && (64 - 2 * opp_cnt <= alpha || 64 - 2 * opp_cnt < beta) ) {
+    int stability_bound;
+    EdgeIndices edges;
+    stability_bound = 64 - 2 * count_edge_stable_indexed( oppcol, opp_bits, my_bits, &edges );
+    if ( stability_bound <= alpha )
+      return alpha;
+    if ( edges.bits != 0 ) {
+      stability_bound = 64 - 2 * count_stable_indexed( oppcol, opp_bits, my_bits, &edges );
+      if ( stability_bound < beta )
+        beta = stability_bound + 1;
+      if ( stability_bound <= alpha )
+        return alpha;
+    }
+  }
+#endif
+
+  int orig_sq[7];
+  int cand[7];
+  int k;
+
+  orig_sq[0] = sq1; orig_sq[1] = sq2; orig_sq[2] = sq3; orig_sq[3] = sq4;
+  orig_sq[4] = sq5; orig_sq[5] = sq6; orig_sq[6] = sq7;
+
+  if ( region_parity != 0 ) {
+    int m_even[7];
+    int n_odd = 0, n_even = 0;
+    int i;
+    for ( i = 0; i < 7; i++ ) {
+      if ( quadrant_mask[orig_sq[i]] & region_parity )
+	cand[n_odd++] = orig_sq[i];
+      else
+	m_even[n_even++] = orig_sq[i];
+    }
+    for ( i = 0; i < n_even; i++ )
+      cand[n_odd + i] = m_even[i];
+  }
+  else {
+    for ( k = 0; k < 7; k++ )
+      cand[k] = orig_sq[k];
+  }
+
+  for ( k = 0; k < 7; k++ ) {
+    int m = cand[k];
+    flipped = TestFlips_wrapper( m, my_bits, opp_bits );
+    if ( flipped != 0 ) {
+      int rem[6];
+      int r = 0, j;
+      for ( j = 0; j < 7; j++ ) {
+	if ( orig_sq[j] != m )
+	  rem[r++] = orig_sq[j];
+      }
+      FULL_ANDNOT( new_opp_bits, opp_bits, bb_flips );
+      new_disc_diff = -disc_diff - 2 * flipped - 1;
+      region_parity ^= quadrant_mask[m];
+#if USE_SHALLOW_TT
+      unsigned int diff1, diff2;
+      end_hash_diff( bb_flips, my_bits, color, m, &diff1, &diff2 );
+      hash1 ^= diff1;
+      hash2 ^= diff2;
+#endif
+      ev = -solve_six_empty( new_opp_bits, bb_flips,
+			     rem[0], rem[1], rem[2], rem[3], rem[4], rem[5],
+			     -beta, -alpha, oppcol, new_disc_diff, TRUE );
+#if USE_SHALLOW_TT
+      hash1 ^= diff1;
+      hash2 ^= diff2;
+#endif
+      region_parity ^= quadrant_mask[m];
+      if ( ev >= beta ) {
+	end_best_move = m;
+#if USE_SHALLOW_TT
+	add_shallow_hash( ev, m, ENDGAME_SCORE | LOWER_BOUND, 7 );
+#endif
+	return ev;
+      }
+      else if ( ev > score ) {
+	score = ev;
+	if ( score > alpha )
+	  alpha = score;
+	best_sq = m;
+      }
+    }
+  }
+
+  if ( score == -INFINITE_EVAL ) {
+    if ( !pass_legal ) {  /* Seven empty squares */
+      if ( disc_diff > 0 )
+	return disc_diff + 7;
+      if ( disc_diff < 0 )
+	return disc_diff - 7;
+      return 0;
+    }
+    else {
+      hash1 ^= hash_flip_color1;
+      hash2 ^= hash_flip_color2;
+      ev = -solve_seven_empty( opp_bits, my_bits,
+			       orig_sq[0], orig_sq[1], orig_sq[2], orig_sq[3], orig_sq[4], orig_sq[5], orig_sq[6],
+			       -beta, -alpha, oppcol, -disc_diff, FALSE );
+      hash1 ^= hash_flip_color1;
+      hash2 ^= hash_flip_color2;
+      return ev;
+    }
+  }
+
+#if USE_SHALLOW_TT
+  if ( score <= in_alpha )
+    add_shallow_hash( score, best_sq, ENDGAME_SCORE | UPPER_BOUND, 7 );
+  else
+    add_shallow_hash( score, best_sq, ENDGAME_SCORE | EXACT_VALUE, 7 );
+#endif
+
+  return score;
+}
+
+
+
+static int
 solve_parity( BitBoard my_bits,
 	      BitBoard opp_bits,
 	      int alpha,
@@ -1234,6 +1393,18 @@ solve_parity( BitBoard my_bits,
   int new_disc_diff;
   int sq, old_sq, best_sq = 0;
   unsigned int parity_mask;
+
+  if ( empties == 7 ) {
+    int sq1 = end_move_list[END_MOVE_LIST_HEAD].succ;
+    int sq2 = end_move_list[sq1].succ;
+    int sq3 = end_move_list[sq2].succ;
+    int sq4 = end_move_list[sq3].succ;
+    int sq5 = end_move_list[sq4].succ;
+    int sq6 = end_move_list[sq5].succ;
+    int sq7 = end_move_list[sq6].succ;
+    return solve_seven_empty( my_bits, opp_bits, sq1, sq2, sq3, sq4, sq5, sq6, sq7,
+			      alpha, beta, color, disc_diff, pass_legal );
+  }
 
   if ( empties == 6 ) {
     int sq1 = end_move_list[END_MOVE_LIST_HEAD].succ;
